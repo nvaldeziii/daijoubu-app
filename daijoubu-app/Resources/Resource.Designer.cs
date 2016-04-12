d9f71e845cb7f5d4b83d98a895006e974eb82ac9
Noli Valdez III <nolivaldeziii@gmail.com>
2016-04-12 18:57:02 +0800

    Created some Initial GUI files
    1. Typing Module
    2. Multiple Choise Module
    3. Listening Module

--- daijoubu-app/Resources/Resource.Designer.cs
+++ daijoubu-app/Resources/Resource.Designer.cs
@@ -63,6 +63,39 @@ namespace daijoubu_app
 			// aapt resource value: 0x7f050000
 			public const int MyButton = 2131034112;
 			
+			// aapt resource value: 0x7f050001
+			public const int button1 = 2131034113;
+			
+			// aapt resource value: 0x7f050007
+			public const int button_mc_q1 = 2131034119;
+			
+			// aapt resource value: 0x7f050008
+			public const int button_mc_q2 = 2131034120;
+			
+			// aapt resource value: 0x7f050009
+			public const int button_mc_q3 = 2131034121;
+			
+			// aapt resource value: 0x7f05000a
+			public const int button_mc_q4 = 2131034122;
+			
+			// aapt resource value: 0x7f050003
+			public const int editText1 = 2131034115;
+			
+			// aapt resource value: 0x7f050002
+			public const int space1 = 2131034114;
+			
+			// aapt resource value: 0x7f050006
+			public const int space_item_bottom = 2131034118;
+			
+			// aapt resource value: 0x7f050004
+			public const int space_item_top = 2131034116;
+			
+			// aapt resource value: 0x7f05000b
+			public const int textView_item = 2131034123;
+			
+			// aapt resource value: 0x7f050005
+			public const int textView_mc_item = 2131034117;
+			
 			static Id()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
@@ -78,6 +111,15 @@ namespace daijoubu_app
 			
 			// aapt resource value: 0x7f030000
 			public const int Main = 2130903040;
+			
+			// aapt resource value: 0x7f030001
+			public const int Module_Listening = 2130903041;
+			
+			// aapt resource value: 0x7f030002
+			public const int Module_MultipleChoise = 2130903042;
+			
+			// aapt resource value: 0x7f030003
+			public const int Module_Typing = 2130903043;
 			
 			static Layout()
 			{
@@ -97,6 +139,9 @@ namespace daijoubu_app
 			
 			// aapt resource value: 0x7f040000
 			public const int Hello = 2130968576;
+			
+			// aapt resource value: 0x7f040002
+			public const int Icon_Speaker = 2130968578;
 			
 			static String()
 			{
