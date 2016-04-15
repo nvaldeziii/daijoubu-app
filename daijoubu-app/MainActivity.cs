8e2fb4dd5c837c4f2a7a0947548e4a91dee770b5
Noli Valdez III <nolivaldeziii@gmail.com>
2016-04-15 18:01:15 +0800

    Hamburger icon now shows on top left corner

--- daijoubu-app/MainActivity.cs
+++ daijoubu-app/MainActivity.cs
@@ -8,6 +8,7 @@ using Android.OS;
 using System.Collections.Generic;
 using AndroidHelper;
 using SupportToolbar = Android.Support.V7.Widget.Toolbar;
+using Android.Support.V4.Widget;
 using Android.Support.V7.App;
 
 namespace daijoubu_app
@@ -17,6 +18,9 @@ namespace daijoubu_app
     {
 
         private SupportToolbar mToolbar;
+        private MyActionBarDrawerToggle mDrawerToggle;
+        private DrawerLayout mDrawerLayout;
+        private ListView mLeftDrawer;
 
         protected override void OnCreate(Bundle bundle)
         {
@@ -26,17 +30,31 @@ namespace daijoubu_app
             SetContentView(Resource.Layout.Main);
 
             //FragmentTransaction Frag = FragmentManager.BeginTransaction();
-
             //Frag.Add(Resource.Id.FragmentContainer, new MenuFrag(this), "MenuFrag");
             //Frag.Commit();
 
             //toolbar
             mToolbar = FindViewById<SupportToolbar>(Resource.Id.toolbar);
+            mDrawerLayout = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
+            mLeftDrawer = FindViewById<ListView>(Resource.Id.left_drawer);
+
             SetSupportActionBar(mToolbar);
-           
+
+            mDrawerToggle = new MyActionBarDrawerToggle(this, mDrawerLayout, Resource.String.openDrawer, Resource.String.closeDrawer);
+
+            mDrawerLayout.SetDrawerListener(mDrawerToggle);
+            SupportActionBar.SetDisplayHomeAsUpEnabled(true);
+            //SupportActionBar.SetHomeButtonEnabled(true);
+            SupportActionBar.SetDisplayShowTitleEnabled(true);
+            mDrawerToggle.SyncState();
         }
 
-       
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            mDrawerToggle.OnOptionsItemSelected(item);
+            return base.OnOptionsItemSelected(item);
+        }
     }
 }
 
