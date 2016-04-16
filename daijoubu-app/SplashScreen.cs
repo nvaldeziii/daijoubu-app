d11e436a2905c69cff8c80b30c47d7685084c06c
Noli Valdez III <nolivaldeziii@gmail.com>
2016-04-17 01:33:32 +0800

    Recoded splash screen as per xamarin tutorial

--- daijoubu-app/SplashScreen.cs
+++ daijoubu-app/SplashScreen.cs
@@ -10,18 +10,35 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using System.Threading.Tasks;
 
 namespace daijoubu_app
 {
     [Activity(Theme = "@style/MyTheme.Splash", MainLauncher =true,NoHistory =true)]
     public class SplashScreen : Activity
     {
-        protected override void OnCreate(Bundle savedInstanceState)
+        public override void OnCreate(Bundle savedInstanceState, PersistableBundle persistentState)
         {
-            base.OnCreate(savedInstanceState);
+            base.OnCreate(savedInstanceState, persistentState);
 
-            //Thread.Sleep(4000);
-            StartActivity(typeof(MainActivity));
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+
+            Task startupWork = new Task(() => {
+                //Log.Debug(TAG, "Performing some startup work that takes a bit of time.");
+                Task.Delay(5000);  // Simulate a bit of startup work.
+                //Log.Debug(TAG, "Working in the background - important stuff.");
+            });
+
+            startupWork.ContinueWith(t => {
+                //Log.Debug(TAG, "Work is finished - start Activity1.");
+                StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+            }, TaskScheduler.FromCurrentSynchronizationContext());
+
+            startupWork.Start();
         }
     }
 }
